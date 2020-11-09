3dc409220ac4c6b3f75b7443b2641a78fe66d326
Samuel Jungman <jungmans@iastate.edu>
2020-11-09 02:17:06 -0600

    Initial UI setup

--- app/src/main/java/com/example/proj2/MainActivity.java
+++ app/src/main/java/com/example/proj2/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.proj2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button profileButton = (Button) findViewById(R.id.profile);
+        Button myListings = (Button) findViewById(R.id.myList);
+        Button otherListings = (Button) findViewById(R.id.otListings);
+
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+
+        myListings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+
+        otherListings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
     }
 }
